a0ab9755309bea75b6a15582b27eb62ebde837e8
Biel Simon Rojas <bielsimon2@gmail.com>
2018-08-11 18:09:18 +0200

    Updated tests: Spelled contents css display property correctly

--- src/Switch.test.tsx
+++ src/Switch.test.tsx
@@ -22,19 +22,19 @@ describe('Switch', () => {
         const component = shallow(
             <Switch views={[<C1/>, <C2/>]} selected={0}/>
         )
-        expect(component.html()).toEqual('<div style="display:content"><div>C1</div></div><div style="display:none"><div>C2</div></div>')
+        expect(component.html()).toEqual('<div style="display:contents"><div>C1</div></div><div style="display:none"><div>C2</div></div>')
     })
     it('shows multiple when multiple are selected', () => {
         const component = shallow(
             <Switch views={[<C1/>, <C2/>]} selected={[0,1]}/>
         )
-        expect(component.html()).toEqual('<div style="display:content"><div>C1</div></div><div style="display:content"><div>C2</div></div>')
+        expect(component.html()).toEqual('<div style="display:contents"><div>C1</div></div><div style="display:contents"><div>C2</div></div>')
     })
     it("doesn't crash when an index is out of bounds", () => {
         const component = shallow(
             <Switch views={[<C1/>, <C2/>]} selected={[0,1,2]}/>
         )
-        expect(component.html()).toEqual('<div style="display:content"><div>C1</div></div><div style="display:content"><div>C2</div></div>')
+        expect(component.html()).toEqual('<div style="display:contents"><div>C1</div></div><div style="display:contents"><div>C2</div></div>')
     })
     it('works using children', () => {
         const component = shallow(
