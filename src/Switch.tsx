cbff11b2249e051ad8c0e5e08d82d5d5063d77d6
Biel Simon Rojas <bielsimon2@gmail.com>
2018-08-11 18:07:01 +0200

    Spelled contents css display property correctly

--- src/Switch.tsx
+++ src/Switch.tsx
@@ -36,7 +36,7 @@ class Switch extends PureComponent<SwitchProps> {
         let views = this.getViews();
         let renderedViews = views.map((view, index) => {
             const selected = _.some(this.getSelectedIndexes(), (selectedIndex) => index === selectedIndex)
-            const display = selected ? 'content' : 'none';
+            const display = selected ? 'contents' : 'none';
             const View: any = view;
             const specificProps = this.props.props
             return <div key={index} style={{display}}>
