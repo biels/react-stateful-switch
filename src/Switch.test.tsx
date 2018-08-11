b290cd92c0c3bc3151d99dcef3b756b0bd9a85f4
Biel Simon Rojas <bielsimon2@gmail.com>
2018-08-11 17:00:43 +0200

    Added Switch component and tests

---
+++ src/Switch.test.tsx
@@ -0,0 +1,49 @@
+import * as React from "react";
+import * as renderer from 'react-test-renderer';
+import Switch from "./Switch";
+import { shallow } from 'enzyme'
+
+describe('Switch', () => {
+    it('renders nothing by default', () => {
+        const component = shallow(
+            <Switch/>
+        )
+        expect(component.html()).toMatch('')
+    })
+    const C1 = (props) => <div>C1</div>
+    const C2 = (props) => <div>C2</div>
+    it('renders all hidden when none is selected', () => {
+        const component = shallow(
+            <Switch views={[<C1/>, <C2/>]}/>
+        )
+        expect(component.html()).toEqual('<div style="display:hidden"><div>C1</div></div><div style="display:hidden"><div>C2</div></div>')
+    })
+    it('shows first when first is selected without array', () => {
+        const component = shallow(
+            <Switch views={[<C1/>, <C2/>]} selected={0}/>
+        )
+        expect(component.html()).toEqual('<div style="display:content"><div>C1</div></div><div style="display:hidden"><div>C2</div></div>')
+    })
+    it('shows multiple when multiple are selected', () => {
+        const component = shallow(
+            <Switch views={[<C1/>, <C2/>]} selected={[0,1]}/>
+        )
+        expect(component.html()).toEqual('<div style="display:content"><div>C1</div></div><div style="display:content"><div>C2</div></div>')
+    })
+    it("doesn't crash when an index is out of bounds", () => {
+        const component = shallow(
+            <Switch views={[<C1/>, <C2/>]} selected={[0,1,2]}/>
+        )
+        expect(component.html()).toEqual('<div style="display:content"><div>C1</div></div><div style="display:content"><div>C2</div></div>')
+    })
+    it('works using children', () => {
+        const component = shallow(
+            <Switch selected={[1]}>
+                <C1/>
+                <C2/>
+            </Switch>
+        )
+        expect(component.html()).toEqual('<div style="display:hidden"><div>C1</div></div><div style="display:content"><div>C2</div></div>')
+    })
+
+})
---
+++ src/Switch.tsx
@@ -0,0 +1,53 @@
+import * as React from 'react';
+import {ComponentType, PureComponent, ReactNode} from 'react';
+import * as _ from "lodash";
+
+export type View = ReactNode | Function
+
+export interface ViewProps {
+
+}
+
+export interface SwitchProps {
+    selected?: number | number[] | null
+    views?: View[]
+    props?: ViewProps | ViewProps[]
+    children?: ReactNode
+}
+
+class Switch extends PureComponent<SwitchProps> {
+    static defaultProps: Partial<SwitchProps> = {
+        selected: null
+    }
+    getSelectedIndexes = () => {
+        if (!_.isArray(this.props.selected)) {
+            return [this.props.selected];
+        }
+        return this.props.selected;
+    }
+    getViews = (): View[] => {
+        //Get views from props
+        let views: any = this.props.views || this.props.children || [];
+        if (!_.isArray(views)) views = [views]
+        return views;
+    }
+
+    render() {
+        let views = this.getViews();
+        let renderedViews = views.map((view, index) => {
+            const selected = _.some(this.getSelectedIndexes(), (selectedIndex) => index === selectedIndex)
+            const display = selected ? 'content' : 'hidden';
+            const View: any = view;
+            const specificProps = this.props.props
+            return <div key={index} style={{display}}>
+                {view}
+            </div>;
+        })
+        if(renderedViews === []) return null;
+        return <React.Fragment>
+            {renderedViews}
+        </React.Fragment>
+    }
+}
+
+export default Switch;
