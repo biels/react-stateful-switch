a4761b0750b26c796f58ce638f7057d0067c04ec
Biel Simon Rojas <bielsimon2@gmail.com>
2018-08-20 15:43:32 +0200

    Updated tsconfig.json to use esModuleInterop and added functionality to package.json

--- src/Switch.tsx
+++ src/Switch.tsx
@@ -1,6 +1,6 @@
-import * as React from 'react';
+import React from 'react';
 import {ComponentType, PureComponent, ReactNode} from 'react';
-import * as _ from "lodash";
+import _ from "lodash";
 
 export type View = ReactNode | Function
 
