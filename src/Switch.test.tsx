f3aadf9fb007d3ea26aaa7d2ff02eeb6e51ba34e
Biel Simon Rojas <bielsimon2@gmail.com>
2018-09-24 03:38:20 +0200

    Added maxHidden. Tests passing.

--- src/Switch.test.tsx
+++ src/Switch.test.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import renderer from 'react-test-renderer';
 import Switch from "./Switch";
-import { shallow } from 'enzyme'
+import {shallow, ShallowWrapper} from 'enzyme'
 
 describe('Switch', () => {
     it('renders nothing by default', () => {
@@ -12,6 +12,7 @@ describe('Switch', () => {
     })
     const C1 = (props) => <div>C1</div>
     const C2 = (props) => <div>C2</div>
+    const C3 = (props) => <div>C3</div>
     it('renders all hidden when none is selected', () => {
         const component = shallow(
             <Switch views={[<C1/>, <C2/>]}/>
@@ -45,5 +46,88 @@ describe('Switch', () => {
         )
         expect(component.html()).toEqual('<div style="display:none"><div>C1</div></div><div style="display:contents"><div>C2</div></div>')
     })
+    it('action sequence switching to maxHidden', () => {
+        const component: ShallowWrapper<Switch["props"], Switch["state"], Switch> = shallow(
+            <Switch views={[<C1/>, <C2/>]}/>
+        )
+        expect(component.html()).toEqual('<div style="display:none"><div>C1</div></div><div style="display:none"><div>C2</div></div>')
+        component.setProps({...component.props(), selected: 1})
+        expect(component.html()).toEqual('<div style="display:none"><div>C1</div></div><div style="display:contents"><div>C2</div></div>')
+        component.setProps({...component.props(), selected: 0})
+        expect(component.html()).toEqual('<div style="display:contents"><div>C1</div></div><div style="display:none"><div>C2</div></div>')
+        component.setProps({...component.props(), selected: 0, maxHidden: 0})
+        expect(component.html()).toEqual('<div style="display:contents"><div>C1</div></div>')
+    })
+    it('action sequence using maxHidden 1', () => {
+        // C1 [C2] C3
+        const component: ShallowWrapper<Switch["props"], Switch["state"], Switch> = shallow(
+            <Switch views={[<C1/>, <C2/>, <C3/>]} selected={1} maxHidden={1}/>
+        )
+        expect(component.html()).toEqual('<div style="display:contents"><div>C2</div></div>');
 
+        // C1 (C2) [C3]
+        component.setProps({...component.props(), selected: 2});
+        expect(component.html()).toEqual('<div style="display:none"><div>C2</div></div><div style="display:contents"><div>C3</div></div>');
+
+        // C1 [C2] (C3)
+        component.setProps({...component.props(), selected: 1});
+        expect(component.html()).toEqual('<div style="display:contents"><div>C2</div></div><div style="display:none"><div>C3</div></div>');
+
+        // [C1] (C2) C3
+        component.setProps({...component.props(), selected: 0});
+        expect(component.html()).toEqual('<div style="display:contents"><div>C1</div></div><div style="display:none"><div>C2</div></div>');
+
+
+    })
+    it('action sequence using maxHidden 2', () => {
+        // C1 [C2] C3
+        const component: ShallowWrapper<Switch["props"], Switch["state"], Switch> = shallow(
+            <Switch views={[<C1/>, <C2/>, <C3/>]} selected={1} maxHidden={2}/>
+        )
+        expect(component.html()).toEqual('<div style="display:contents"><div>C2</div></div>');
+
+        // C1 (C2) [C3]
+        component.setProps({...component.props(), selected: 2});
+        expect(component.html()).toEqual('<div style="display:none"><div>C2</div></div><div style="display:contents"><div>C3</div></div>');
+
+        // [C1] (C2) (C3)
+        component.setProps({...component.props(), selected: 0});
+        expect(component.html()).toEqual('<div style="display:contents"><div>C1</div></div><div style="display:none"><div>C2</div></div><div style="display:none"><div>C3</div></div>');
+
+
+    })
+    it('same action sequence using maxHidden 3', () => {
+        // C1 [C2] C3
+        const component: ShallowWrapper<Switch["props"], Switch["state"], Switch> = shallow(
+            <Switch views={[<C1/>, <C2/>, <C3/>]} selected={1} maxHidden={3}/>
+        )
+        expect(component.html()).toEqual('<div style="display:contents"><div>C2</div></div>');
+
+        // C1 (C2) [C3]
+        component.setProps({...component.props(), selected: 2});
+        expect(component.html()).toEqual('<div style="display:none"><div>C2</div></div><div style="display:contents"><div>C3</div></div>');
+
+        // [C1] (C2) (C3)
+        component.setProps({...component.props(), selected: 0});
+        expect(component.html()).toEqual('<div style="display:contents"><div>C1</div></div><div style="display:none"><div>C2</div></div><div style="display:none"><div>C3</div></div>');
+
+
+    })
+    it('action sequence using maxHidden 2 and prefetch', () => {
+        // C1 [C2] C3
+        const component: ShallowWrapper<Switch["props"], Switch["state"], Switch> = shallow(
+            <Switch views={[<C1/>, <C2/>, <C3/>]} selected={1} maxHidden={2}/>
+        )
+        expect(component.html()).toEqual('<div style="display:contents"><div>C2</div></div>');
+
+        // C1 (C2) [C3]
+        component.setProps({...component.props(), selected: 2});
+        expect(component.html()).toEqual('<div style="display:none"><div>C2</div></div><div style="display:contents"><div>C3</div></div>');
+
+        // [C1] (C2) (C3)
+        component.setProps({...component.props(), selected: 0});
+        expect(component.html()).toEqual('<div style="display:contents"><div>C1</div></div><div style="display:none"><div>C2</div></div><div style="display:none"><div>C3</div></div>');
+
+
+    })
 })
--- src/Switch.tsx
+++ src/Switch.tsx
@@ -13,11 +13,14 @@ export interface SwitchProps {
     views?: View[]
     props?: ViewProps | ViewProps[]
     children?: ReactNode
+    short: boolean
+    maxHidden?: number
 }
 
 class Switch extends PureComponent<SwitchProps> {
     static defaultProps: Partial<SwitchProps> = {
-        selected: null
+        selected: null,
+        maxHidden: null
     }
     getSelectedIndexes = () => {
         if (!_.isArray(this.props.selected)) {
@@ -31,19 +34,46 @@ class Switch extends PureComponent<SwitchProps> {
         if (!_.isArray(views)) views = [views]
         return views;
     }
+    displayHistory = [this.props.selected]
+    componentDidMount(){
+        // console.log(`Adding ${this.props.selected} to history`);
+        // this.displayHistory.push(this.props.selected)
+
+    }
+
+    // getSnapshotBeforeUpdate(prevProps: SwitchProps, prevState) {
+    //     console.log(`Adding ${prevProps.selected} to history`);
+    //     this.displayHistory.push(prevProps.selected)
+    // }
 
     render() {
         let views = this.getViews();
-        let renderedViews = views.map((view, index) => {
-            const selected = _.some(this.getSelectedIndexes(), (selectedIndex) => index === selectedIndex)
+        let selectedIndexes = this.getSelectedIndexes();
+        let cachedIndexes = this.props.maxHidden != null ?
+            _.slice(_.reverse(_.uniq(_.reverse(_.flatten(this.displayHistory)))), -1 * this.props.maxHidden) :
+            _.range(views.length);
+        if(this.props.maxHidden === 0) cachedIndexes = []
+        // const renderedIndexes = _.uniq(_.flatten(cachedIndexes.concat(selectedIndexes)))
+        console.log(`$(Sel: ${selectedIndexes} / Cached: ${cachedIndexes}), Hist: ${this.displayHistory}`);
+        let renderedViews = _.range(views.length).map((index) => {
+            const view = views[index]
+            let selectedIndexes = this.getSelectedIndexes();
+
+            const selected = _.some(selectedIndexes, (selectedIndex) => index === selectedIndex)
+            if(!selected && !cachedIndexes.includes(index)) {
+                return null;
+            }
             const display = selected ? 'contents' : 'none';
             const View: any = view;
             const specificProps = this.props.props
             return <div key={index} style={{display}}>
                 {view}
             </div>;
-        })
-        if(renderedViews === []) return null;
+        }).filter(rv => rv != null);
+        if (renderedViews === []) return null;
+
+        if(this.props.selected !=  null && this.props.selected != _.last(this.displayHistory))this.displayHistory.push(this.props.selected)
+
         return <React.Fragment>
             {renderedViews}
         </React.Fragment>
